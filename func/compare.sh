e384ad551890f3dc9d4dda4633b99dc003cce311
Peilin Lai <laipl@caen-vnc-mi09.engin.umich.edu>
2023-11-22 01:42:37 -0500

    Complete the compare.sh

--- func/compare.sh
+++ func/compare.sh
@@ -2,26 +2,61 @@
 
 set -eu
 
-if [ $# -ne 1 ]; then
-    echo "Usage: ./compare.sh <compare_content = wb=1, out=2, all=3>"
+if [ $# -ne 2 ]; then
+    echo "Usage: ./compare.sh <test_name> <compare_content = wb=1, out=2, all=3>"
     exit 1
 fi
-DIFF_TYPE=$1
-if [ $DIFF_TYPE -ne 1 ]; then
-    diff -u ../correct_output/p3_mult_no_lsq.out ../output/mult_no_lsq.out > ../output/out_diff.out.txt | true
-    LINE_NUM=`cat ../output/out_diff.out.txt | wc -l`
-    if [ $LINE_NUM -eq 1 ]; then
-        echo "@@@Output File is correct"
-    else
-        echo "@@@Output File is wrong"
-    fi
+
+TEST_NAME=$1
+DIFF_TYPE=$2
+
+FILE_TYPE="wb"
+if [ $DIFF_TYPE -eq 2 ]; then
+    FILE_TYPE="out"
 fi
-if [ $DIFF_TYPE -ne 0 ]; then
-    diff -u ../correct_output/p3_mult_no_lsq.wb ../output/mult_no_lsq.wb > ../output/wb_diff.wb.txt | true
-    LINE_NUM=`cat ../output/wb_diff.wb.txt | wc -l`
-    if [ $LINE_NUM -eq 0 ]; then
-        echo "@@@Write back File is correct"
-    else
-        echo "@@@Write back File is wrong"
+
+# Check if test file in final_project
+P4_TEST_EXIST=`ls -l ../programs | grep $TEST_NAME | wc -l`
+if [ $P4_TEST_EXIST -eq 0 ]; then
+    echo "Test $TEST_NAME does not exist"
+    exit 1
+fi
+
+# Find if the correct output file exists
+if [ ! -f ../correct_output/$TEST_NAME.out ]; then
+    cd ../../project03
+
+    # Check if the test file in project03
+    TEST_EXTSTED=`ls -l programs/ | grep $TEST_NAME | wc -l`
+    if [ $TEST_EXTSTED -eq 0 ]; then
+        cp ../../final_project/programs/$TEST_NAME.s programs/$TEST_NAME.s
     fi
+
+    # Get the correct output
+    make $TEST_NAME.out
+
+    # Move the correct output to final_project
+    mv output/$TEST_NAME.out  ../final_project/correct_output/$TEST_NAME.out
+    mv output/$TEST_NAME.wb   ../final_project/correct_output/$TEST_NAME.wb
+    mv output/$TEST_NAME.ppln ../final_project/correct_output/$TEST_NAME.ppln
+
+    # Go Back
+    cd ../final_project
+fi
+
+# Find if the output file exists
+if [ ! -f ../output/$TEST_NAME.out ]; then
+    # Get the correct output
+    make $TEST_NAME.out
+fi
+
+# Compare
+diff -u ../correct_output/$TEST_NAME.$FILE_TYPE ../output/$TEST_NAME.$FILE_TYPE > ../output/$TEST_NAME.$FILE_TYPE.out.txt | true
+
+# Print the result
+LINE_NUM=`cat ../output/$TEST_NAME.$FILE_TYPE.out.txt | wc -l`
+if [ $LINE_NUM -eq 0 ]; then
+    echo "@@@ $FILE_TYPE File is correct"
+else
+    echo "@@@ $FILE_TYPE File is wrong"
 fi
